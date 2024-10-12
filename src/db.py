3e4b40fd66889cca04f80b2b3cde574b573d5001
Sabuka-Dumbo <anonym.sas@proton.me>
2024-10-12 15:23:15 +0000

    Oct 12, 2024, 7:23 PM

--- src/db.py
+++ src/db.py
@@ -3,10 +3,12 @@ import sqlite3
 DB_SCHEMA = """
 CREATE TABLE Places IF NOT EXSIST
 (
-    id CHAR(22),
-    latitude Decimal(8,6),
-    longitude Decimal(9,6),
-    creation TIMESTAMP
+    id          CHAR(22),
+    creator     CHAR(255),
+    latitude    Decimal(8,6),
+    longitude   Decimal(9,6),
+    creation    TIMESTAMP,
+    rating      INT(255),
 );
 
 
