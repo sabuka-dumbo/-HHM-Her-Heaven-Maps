c51bd49ff55facc64802c2bcaa2bb9cffe918590
Sabuka-Dumbo <anonym.sas@proton.me>
2024-10-12 15:03:11 +0000

    Oct 12, 2024, 7:03 PM

---
+++ src/db.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+DB_SCHEMA = """
+CREATE TABLE Places IF NOT EXSIST
+(
+    id CHAR(22),
+    latitude Decimal(8,6),
+    longitude Decimal(9,6),
+    creation
+);
+
+
+
+"""
+
+conn = sqlite3.connect("db.sqlite")
