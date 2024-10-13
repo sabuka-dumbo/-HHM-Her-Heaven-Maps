8c0b0be4b1307fa1ff28b0c5da32acde43564e32
Sabuka-Dumbo <anonym.sas@proton.me>
2024-10-13 13:15:30 +0000

    Oct 13, 2024, 5:15 PM

--- HHM/Maps/models.py
+++ HHM/Maps/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    country = models.CharField(max_length=300, default="0")
+    city = models.CharField(max_length=300, default="0")
+
+class Rate(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poster")
+    rate = models.CharField(max_length=300)
+    place = models.CharField(max_length=600)
+
+    def __str__(self):
+        return f"{self.user}, rated place"
