b04d4d53295d23e7c2f451bb35823917cab900a1
Sabuka-Dumbo <anonym.sas@proton.me>
2024-10-13 07:09:30 +0000

    Oct 13, 2024, 11:09 AM

--- src/__init__.py
+++ src/__init__.py
@@ -7,6 +7,15 @@ def create_app(test_config:typing.Union[None, dict] =None):
     # create and configure the app
     # (Application factory)
     app = Flask(__name__)
+    # Secret key is used to encrypt the session cookie
+    app.secret_key = os.urandom(32)
+
+    app.config['SESSION_TYPE'] = 'redis'
+    app.config['SESSION_PERMANENT'] = False
+    app.config['SESSION_USE_SIGNER'] = True
+    app.config['SESSION_REDIS'] = redis.from_url('redis://127.0.0.1:6379')
+
+    # yo main part of register is done
 
     # If test_config is supplied, it most likely mean we are running tests
     if test_config:
@@ -15,7 +24,6 @@ def create_app(test_config:typing.Union[None, dict] =None):
         config = dotenv.dotenv_values()
         # this read the env file, and use the config as flask config
         # We will use flask config as config for everything
-
         app.config.from_mapping(config)
 
     
